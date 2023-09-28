d1133e542fe78b4eb0343c4fb96d502bdc649eab
pdfbMarcos <marcos.ignacio@bol.com.br>
2023-09-28 16:28:51 +0100

    testes de integração Signfinant API

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -8,12 +8,39 @@ import useIdle from "../hooks/useIdleTimer";
 import { AuthContext } from "../contexts/auth";
 import "../assets/styles/home.css";
 
+import axios from "axios";
+
 function HomePage() {
   const navigate = useNavigate();
   const [showModal, setShowModal] = useState(false);
   const [remainingTime, setRemainingTime] = useState(0);
 
   const { logout } = useContext(AuthContext);
+
+  // Envelope status
+  // https://www.significant.com/Api/v6/envelope/43b4ad5e-9022-4860-9116-944af17258d3
+  // ID do documento
+  // https://www.significant.com/Api/v6/envelope/43b4ad5e-9022-4860-9116-944af17258d3/files
+  // Download documento
+  // https://www.significant.com/Api/v6/file/5fff35f3-5939-453b-89aa-fa400e27262d
+
+  async function handleGet() {
+    const cors_api_url = "https://cors-anywhere.herokuapp.com/";
+    await axios
+      .get(
+        cors_api_url +
+          `https://www.significant.com/Api/v6/envelope/43b4ad5e-9022-4860-9116-944af17258d3/files`,
+        {
+          headers: {
+            ApiToken:
+              "1wl7z1orjjyuaab6f9yi1p8ch0hw3sg2py0jhjbbzxvni9wccvpqpym176bwzzjn",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
+  }
 
   const handleIdle = () => {
     setShowModal(true); //show modal
@@ -53,7 +80,7 @@ function HomePage() {
         <img src={docImg} width="200" height="200" alt="" />
         <h1>Envio de documentos</h1>
         <p>Enviar documentos para assinatura!</p>
-        <a href="#" className="btn btn-primary btn-lg">
+        <a href="#" className="btn btn-primary btn-lg" onClick={handleGet}>
           Enviar
         </a>
       </div>
